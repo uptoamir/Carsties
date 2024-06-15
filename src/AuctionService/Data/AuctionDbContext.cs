4cce0c908f97748424fb7a5f759c3ebc67d1e54e
amirhosseinkhoshbin <khoshbinamirhosein@gmail.com>
2024-06-15 10:35:40 -0400

    End of section 2

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,17 @@
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+    
+    
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,35 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+app.Run();
